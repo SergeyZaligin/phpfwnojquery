0b90c4c53d0d293ff8b2fcf25a9bf7e20774b705
sergey <sergey>
2018-10-22 08:41:27 +0300

    fix

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
  
-  <link rel="preload" rel="preload" href="/css/main.min.css" as="style" onload="this.rel='stylesheet'">
+  <link rel="stylesheet" href="/css/main.min.css">
   <?=View::getMeta(); ?>
 </head>
 <body>
